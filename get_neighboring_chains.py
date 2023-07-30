273bb790f70cc6b7a74dfa1fe34f0fb04191d45e
Matlab Pipeline User <pipeline@rnaprod1.bgsu.edu>
2023-07-30 16:12:10 -0400

    Added additional logic to handle naming messenger RNAs

--- get_neighboring_chains.py
+++ get_neighboring_chains.py
@@ -1,9 +1,16 @@
 from contextlib import contextmanager
-from models import UnitCenter, ChainInfo
+from models import UnitCenter, ChainInfo, ChainPropertyValue
 from database import db_session
 from sqlalchemy import tuple_
+from sqlalchemy.orm import aliased
 import math
 import timeit
+
+test_data = ['4V9P|1|HA|G|1405,4V9P|1|HA|U|1406,4V9P|1|HA|C|1407,4V9P|1|HA|A|1408,4V9P|1|HA|C|1409,4V9P|1|HA|G|1491,4V9P|1|HA|A|1492,4V9P|1|HA|A|1493,4V9P|1|HA|G|1494,4V9P|1|HA|U|1495,4V9P|1|HA|C|1496', '6I7V|1|AA|G|1405,6I7V|1|AA|U|1406,6I7V|1|AA|5MC|1407,6I7V|1|AA|A|1408,6I7V|1|AA|C|1409,6I7V|1|AA|G|1491,6I7V|1|AA|A|1492,6I7V|1|AA|A|1493,6I7V|1|AA|G|1494,6I7V|1|AA|U|1495,6I7V|1|AA|C|1496']
+test_data = ['6I7V|1|AA|G|1405,6I7V|1|AA|U|1406,6I7V|1|AA|5MC|1407,6I7V|1|AA|A|1408,6I7V|1|AA|C|1409,6I7V|1|AA|G|1491,6I7V|1|AA|A|1492,6I7V|1|AA|A|1493,6I7V|1|AA|G|1494,6I7V|1|AA|U|1495,6I7V|1|AA|C|1496']
+
+# List of substrings to check for
+SUBSTRINGS_TO_CHECK = ['mrna', 'messenger rna', 'messenger-rna']
 
 def filter_neighboring_residues(centers_coord, potential_neighboring_units, distance, nt_ids):
     # use the x,y,z coordinates in centers_coord to check if x,y,z coordinates in query results
@@ -147,6 +154,66 @@ def get_chain_name(chains_list):
     else:
         return None
 
+def get_chain_name_revised(pdb_chain_list):
+    # Create aliases for the models to differentiate them in the query
+    cpv = aliased(ChainPropertyValue)
+    ci = aliased(ChainInfo)
+
+    if pdb_chain_list:
+        with db_session() as session:
+            chain_data_list = []
+
+            for pdb_id, chain in pdb_chain_list:
+                # Query the ChainPropertyValue table for 'unp_name' or 'standardized_name'
+                q_cpv_unp_name = session.query(cpv.value.label('NAME'), cpv.chain.label('CHAIN')).\
+                    filter(cpv.pdb_id == pdb_id, cpv.chain == chain, cpv.property.in_(['unp_name', 'standardized_name'])).\
+                    first()
+
+                if q_cpv_unp_name:
+                    # If 'unp_name' or 'standardized_name' is found, add the result directly to chain_data_list
+                    chain_data = (str(q_cpv_unp_name.CHAIN), str(q_cpv_unp_name.NAME))
+                    chain_data_list.append(chain_data)
+                else:
+                    # If 'unp_name' or 'standardized_name' is not found, get the name from the ChainInfo table
+                    q_ci = session.query(ci.compound.label('NAME'), ci.chain_name.label('CHAIN')).\
+                        filter(ci.pdb_id == pdb_id, ci.chain_name == chain).\
+                        first()
+
+                    if q_ci:
+                        # If name is found in the ChainInfo table, add it to chain_data_list
+                        chain_data = (str(q_ci.CHAIN), str(q_ci.NAME))
+                        chain_data_list.append(chain_data)
+
+            return chain_data_list if chain_data_list else []
+    else:
+        return []
+
+
+def contains_mrna_substring(lst):
+    for tup in lst:
+        # Convert the second element of the tuple to lowercase
+        second_element = tup[1].lower()
+        
+        # Check if any substring from the substrings_to_check list is present in the second_element
+        if any(substring in second_element for substring in SUBSTRINGS_TO_CHECK):
+            return True
+    
+    # If no tuple satisfies the condition, return False
+    return False
+
+def replace_with_mRNA(lst):
+    for index, tup in enumerate(lst):
+        # Convert the second element of the tuple to lowercase
+        second_element = tup[1].lower()
+        
+        # Check if any substring from the substrings_to_check list is present in the second_element
+        if any(substring in second_element for substring in SUBSTRINGS_TO_CHECK):
+            # Replace the second element with "mRNA"
+            lst[index] = (tup[0], "mRNA")
+    
+    return lst
+
+
 def get_chains(unit_ids, distance=10):
     # Starting with unit_ids, find the x,y,z coordinates of their centers,
     # expand by distance to a rectangular box around them,
@@ -185,10 +252,15 @@ def get_chains(unit_ids, distance=10):
     # calculate distances to units in unit_ids, record the smallest
     neighboring_residues = filter_neighboring_residues(centers_xyz_coord, potential_neighboring_units, distance, unit_ids)
     neighboring_chains = get_possible_chains_list(neighboring_residues, chain)
-    neighboring_chains_with_names = get_chain_name(neighboring_chains)
+    neighboring_chains_with_names = get_chain_name_revised(neighboring_chains)
 
-    # return neighboring_residues
-    return neighboring_chains_with_names
+    if contains_mrna_substring(neighboring_chains_with_names):
+        formatted_names = replace_with_mRNA(neighboring_chains_with_names)
+    else:
+        formatted_names = neighboring_chains_with_names
+
+    return sorted(formatted_names, key=lambda x: x[1])
+    # return neighboring_chains_with_names
 
 
 def test_run(units_list):
@@ -207,3 +279,11 @@ def test_run(units_list):
 # # print(len(test_units))
 # result, result_time = test_run(test_units)
 # print result_time
+
+# tst = test_run(test_data)
+# print tst
+
+units = ["5J7L|1|AA|G|1491,5J7L|1|AA|A|1492,5J7L|1|AA|A|1493,5J7L|1|AA|G|1494,5J7L|1|AA|U|1495,5J7L|1|AA|C|1496"]
+units = ['4V9C|1|CA|C|1407,4V9C|1|CA|A|1408,4V9C|1|CA|C|1409,4V9C|1|CA|G|1491,4V9C|1|CA|A|1492,4V9C|1|CA|A|1493,4V9C|1|CA|G|1494']
+tst = test_run(units)
+print(tst)
